e7b5ac6b1dccb2577d20b3c17c342eba36e06648
narvas12 <ebukaemmanuel71@gmail.com>
2023-05-26 02:55:43 +0100

    static

--- padech360/settings.py
+++ padech360/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,9 +126,10 @@ if DEBUG:
         BASE_DIR / "static"
     ]
 else:
-    STATIC_ROOT = BASE_DIR / "statics"
+    STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
